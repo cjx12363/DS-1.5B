82285ee8373872a3ce3ed989c7e30b77f0b7a6d4
cjx12363 <2628454243@qq.com>
2026-06-02 20:14:40 +0800

    Save .mat directly to E:\\cjx12363\\LLM4CP-DS

--- dataset_generation/Generate_CSI.m
+++ dataset_generation/Generate_CSI.m
@@ -92,7 +92,7 @@ for iter_Speed=1:length(Speed)
         H_D_pre(iter_Speed,ii,:,:,:,:,:,:)=h(17:20,49:96,:,:,:,:); % future downlink CSI
     end
 end
-save('H_U_his_train.mat', 'H_U_his', '-v7.3');
-save('H_U_pre_train.mat', 'H_U_pre', '-v7.3');
-save('H_D_pre_train.mat', 'H_D_pre', '-v7.3');
-fprintf('Saved to: %s\n', pwd);
+save('E:\\cjx12363\\LLM4CP-DS\\H_U_his_train.mat', 'H_U_his', '-v7.3');
+save('E:\\cjx12363\\LLM4CP-DS\\H_U_pre_train.mat', 'H_U_pre', '-v7.3');
+save('E:\\cjx12363\\LLM4CP-DS\\H_D_pre_train.mat', 'H_D_pre', '-v7.3');
+fprintf('Saved to E:\\cjx12363\\LLM4CP-DS\\\n');
